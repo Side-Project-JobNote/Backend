149383074c8329e323661130f0472c51732ec3de
hyotatoFrappuccino <hjkiln0@gmail.com>
2025-07-20 17:25:14 +0900

    first commit

---
+++ src/main/java/com/jobnote/JobnoteApplication.java
@@ -0,0 +1,13 @@
+package com.jobnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobnoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobnote/JobnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
