2e07b1c2640bb2e41c6ae69d583c2cc6d53948e5
hyotatoFrappuccino <hjkiln0@gmail.com>
2025-07-27 14:49:19 +0900

    feat: ApplicationForm, Schedule, User Repository 생성

---
+++ src/main/java/com/jobnote/domain/schedule/ScheduleRepository.java
@@ -0,0 +1,11 @@
+package com.jobnote.domain.schedule;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    @Query("select s from Schedule s join fetch s.applicationForm af join fetch af.user where af.user.id = :userId")
+    List<Schedule> findAllByUserId(Long userId);
+}
