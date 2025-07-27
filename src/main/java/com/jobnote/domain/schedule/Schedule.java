9f312d583371e064dad915213884a09464f15515
hyotatoFrappuccino <hjkiln0@gmail.com>
2025-07-27 14:48:13 +0900

    feat: ApplicationForm(지원서), Schedule(일정), User(유저) 엔티티 생성

---
+++ src/main/java/com/jobnote/domain/schedule/Schedule.java
@@ -0,0 +1,50 @@
+package com.jobnote.domain.schedule;
+
+import com.jobnote.domain.applicationForm.ApplicationForm;
+import com.jobnote.domain.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "schedule")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "schedule_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "application_form_id", nullable = false)
+    private ApplicationForm applicationForm;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Lob
+    private String memo;
+
+    @Column(nullable = false)
+    private LocalDateTime dateTime;
+
+    @Builder
+    public Schedule(
+            ApplicationForm  applicationForm,
+            String title,
+            String memo,
+            LocalDateTime dateTime
+    ) {
+        this.applicationForm = applicationForm;
+        this.title = title;
+        this.memo = memo;
+        this.dateTime = dateTime;
+    }
+
+    public boolean isOwner(Long id) {
+        return this.applicationForm.getUser().getId().equals(id);
+    }
+}
---
+++ src/main/java/com/jobnote/domain/user/User.java
@@ -0,0 +1,46 @@
+package com.jobnote.domain.user;
+
+import com.jobnote.domain.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String loginId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String avatarUrl;
+
+    @Builder
+    public User(
+            String loginId,
+            String password,
+            String email,
+            String name,
+            String avatarUrl
+    ) {
+        this.loginId = loginId;
+        this.password = password;
+        this.email = email;
+        this.name = name;
+        this.avatarUrl = avatarUrl;
+    }
+}
