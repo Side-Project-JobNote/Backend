4695e11df9493aaa8a6d94626e8aa2b0b6bc313a
gnsdp99 <rladpgns99@gmail.com>
2025-07-29 15:25:36 +0900

    test: 로그인 통합 테스트

--- src/test/java/com/jobnote/JobnoteApplicationTests.java
+++ src/test/java/com/jobnote/JobnoteApplicationTests.java
@@ -1,15 +1,9 @@
 package com.jobnote;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles(profiles = "test")
 @SpringBootTest
-class JobnoteApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+public abstract class JobnoteApplicationTests {
 }
