276eb5516e611a97c5416313fe6128412758ff59
gnsdp99 <rladpgns99@gmail.com>
2025-07-31 22:17:57 +0900

    chore: 불필요한 코드 삭제

--- src/main/java/com/jobnote/global/common/Constants.java
+++ src/main/java/com/jobnote/global/common/Constants.java
@@ -3,9 +3,7 @@ package com.jobnote.global.common;
 public abstract class Constants {
 
     public static final String CLAIM_NAME_TOKEN_TYPE = "token";
-    public static final String CLAIM_NAME_USER_ID = "userId";
     public static final String CLAIM_NAME_EMAIL = "email";
-    public static final String CLAIM_NAME_ROLE = "role";
     public static final String CLAIM_VALUE_ACCESS_TOKEN = "ACCESS_TOKEN";
     public static final String CLAIM_VALUE_REFRESH_TOKEN = "REFRESH_TOKEN";
     public static final String AUTHORIZATION_TYPE_BEARER = "Bearer ";
