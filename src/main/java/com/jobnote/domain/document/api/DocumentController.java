28ec72f4bd1f4e64199b4eb8251a9aa191670b4e
hyotatoFrappuccino <hjkiln0@gmail.com>
2025-08-03 18:33:07 +0900

    refactor: 컨트롤러 경로 복수형으로 수정

--- src/main/java/com/jobnote/domain/document/api/DocumentController.java
+++ src/main/java/com/jobnote/domain/document/api/DocumentController.java
@@ -17,7 +17,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/document")
+@RequestMapping("/api/v1/documents")
 @RequiredArgsConstructor
 public class DocumentController {
 
