42f290c975dbc0e9218666a5f25ea2397e9b23a7
gnsdp99 <rladpgns99@gmail.com>
2025-08-03 12:55:27 +0900

    refactor: 메일 발송 작업 비동기 처리

--- src/main/java/com/jobnote/mail/MailService.java
+++ src/main/java/com/jobnote/mail/MailService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.mail.javamail.*;
@@ -24,6 +25,7 @@ public class MailService {
     @Value("${spring.mail.username}")
     private String username;
 
+    @Async
     public void sendMail(final MailMessageDto mailMessageDto) {
         SimpleMailMessage message = createMailMessage(mailMessageDto);
 
