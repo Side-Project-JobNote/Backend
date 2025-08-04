c384e58a7de3842af34ce32c2c466456541d1050
gnsdp99 <rladpgns99@gmail.com>
2025-08-04 20:45:53 +0900

    test: 로그인 통합 테스트 코드 수정

--- src/test/java/com/jobnote/auth/LoginFilterApplicationTest.java
+++ src/test/java/com/jobnote/auth/LoginFilterApplicationTest.java
@@ -25,45 +25,72 @@ class LoginFilterApplicationTest extends JobnoteApplicationTests {
     private PasswordEncoder passwordEncoder;
 
     private final String LOGIN_URI = "/login";
-    private final String CORRECT_EMAIL = "testCorrectEmail@test.com";
-    private final String CORRECT_PASSWORD = "testCorrectPassword";
-
-    @BeforeEach
-    void setUp() {
-        userRepository.save(User.signUp(CORRECT_EMAIL, passwordEncoder.encode(CORRECT_PASSWORD), "testNickname"));
-    }
 
     @Nested
     @DisplayName("로그인")
     class Login {
-        @Test
-        @DisplayName("성공 - 토큰 발급")
-        void success_IssueToken() throws Exception {
-            // given
-            UserLoginRequest request = new UserLoginRequest(CORRECT_EMAIL, CORRECT_PASSWORD);
+        @Nested
+        @DisplayName("성공")
+        class Success {
+            final String email = "testCorrectEmail@test.com";
+            final String password = "testCorrectPassword";
+            final String nickname = "testNickname";
 
-            // when
-            ResultActions resultActions = mockMvc.perform(post(LOGIN_URI)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(request)));
+            @Test
+            @DisplayName("GUEST는 토큰 미발급 & PENDING_EMAIL_VERIFICATION 예외 발생")
+            void success_GUEST_NotIssueToken_PENDING_EMAIL_VERIFICATION() throws Exception {
+                // given
+                createGuestUser(email, password, nickname);
+                UserLoginRequest request = new UserLoginRequest(email, password);
 
-            // then
-            resultActions
-                    .andExpect(status().isOk())
-                    .andExpect(cookie().exists(COOKIE_NAME_ACCESS_TOKEN))
-                    .andExpect(cookie().exists(COOKIE_NAME_REFRESH_TOKEN));
+                // when
+                ResultActions resultActions = mockMvc.perform(post(LOGIN_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)));
+
+                // then
+                resultActions
+                        .andExpect(status().isForbidden())
+                        .andExpect(cookie().doesNotExist(COOKIE_NAME_ACCESS_TOKEN))
+                        .andExpect(cookie().doesNotExist(COOKIE_NAME_REFRESH_TOKEN))
+                        .andExpect(jsonPath("$.data").isEmpty())
+                        .andExpect(jsonPath("$.code").value(PENDING_EMAIL_VERIFICATION.getCode()));
+            }
+
+            @Test
+            @DisplayName("MEMBER는 토큰 발급")
+            void success_NotGUEST_IssueToken() throws Exception {
+                // given
+                createMemberUser(email, password, nickname);
+                UserLoginRequest request = new UserLoginRequest(email, password);
+
+                // when
+                ResultActions resultActions = mockMvc.perform(post(LOGIN_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)));
+
+                // then
+                resultActions
+                        .andExpect(status().isOk())
+                        .andExpect(cookie().exists(COOKIE_NAME_ACCESS_TOKEN))
+                        .andExpect(cookie().exists(COOKIE_NAME_REFRESH_TOKEN));
+            }
         }
 
         @Nested
         @DisplayName("실패 - 토큰 미발급")
         class Fail {
+            final String correctEmail = "testCorrectEmail@test.com";
+            final String correctPassword = "testCorrectPassword";
+            final String nickname = "testNickname";
 
             @Test
             @DisplayName("잘못된 이메일")
             void fail_NotIssueToken_InvalidEmail() throws Exception {
                 // given
-                final String WRONG_EMAIL = "testWrongEmail@test.com";
-                UserLoginRequest request = new UserLoginRequest(WRONG_EMAIL, CORRECT_PASSWORD);
+                final String wrongEmail = "testWrongEmail@test.com";
+                createGuestUser(correctEmail, correctPassword, nickname);
+                UserLoginRequest request = new UserLoginRequest(wrongEmail, correctPassword);
 
                 // when
                 ResultActions resultActions = mockMvc.perform(post(LOGIN_URI)
@@ -83,8 +110,9 @@ class LoginFilterApplicationTest extends JobnoteApplicationTests {
             @DisplayName("잘못된 비밀번호")
             void fail_NotIssueToken_InvalidPassword() throws Exception {
                 // given
-                final String WRONG_PASSWORD = "testWrongPassword ";
-                UserLoginRequest request = new UserLoginRequest(CORRECT_EMAIL, WRONG_PASSWORD);
+                final String wrongPassword = "testWrongPassword";
+                createGuestUser(correctEmail, correctPassword, nickname);
+                UserLoginRequest request = new UserLoginRequest(correctEmail, wrongPassword);
 
                 // when
                 ResultActions resultActions = mockMvc.perform(post(LOGIN_URI)
@@ -100,5 +128,18 @@ class LoginFilterApplicationTest extends JobnoteApplicationTests {
                         .andExpect(jsonPath("$.code").value(INVALID_USERNAME_PASSWORD.getCode()));
             }
         }
+    }
+
+    private void createGuestUser(final String email, final String password, final String nickname) {
+        signUp(email, password, nickname);
+    }
+
+    private void createMemberUser(final String email, final String password, final String nickname) {
+        User savedUser = signUp(email, password, nickname);
+        savedUser.accept();
+    }
+
+    private User signUp(final String email, final String password, final String nickname) {
+        return userRepository.save(User.signUp(email, passwordEncoder.encode(password), nickname));
     }
 }
