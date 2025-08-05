201e9f0f7e50607717e23b38b7e6fffe5430a772
gnsdp99 <rladpgns99@gmail.com>
2025-08-05 12:37:45 +0900

    feat: 소셜 로그인 응답 방식 변경

--- src/main/java/com/jobnote/auth/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/jobnote/auth/handler/OAuth2LoginSuccessHandler.java
@@ -1,17 +1,15 @@
 package com.jobnote.auth.handler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jobnote.auth.dto.CustomPrincipal;
 import com.jobnote.auth.token.Token;
 import com.jobnote.auth.token.TokenProvider;
 import com.jobnote.domain.user.domain.UserRole;
 import com.jobnote.domain.user.service.AuthTokenService;
-import com.jobnote.global.common.ResponseCode;
-import com.jobnote.global.util.ResponseUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -24,28 +22,25 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
 
     private final TokenProvider tokenProvider;
     private final AuthTokenService authTokenService;
-    private final ObjectMapper objectMapper;
+
+    @Value("${app.frontend.base-url}")
+    private String frontendBaseUrl;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        CustomPrincipal principal = (CustomPrincipal) authentication.getPrincipal();
+        final CustomPrincipal principal = (CustomPrincipal) authentication.getPrincipal();
+        final Token token = authTokenService.saveAndGetToken(principal.getUserId());
+        tokenProvider.addTokenToCookie(response, token);
 
         if (UserRole.GUEST.getKey().equals(principal.getRole())) {
-            responseTokenAndError(response, principal.getUserId());
+            response.sendRedirect(frontendBaseUrl + signUpRequiredQueryParam(true));
             return;
         }
 
-        responseToken(response, principal.getUserId());
+        response.sendRedirect(frontendBaseUrl + signUpRequiredQueryParam(false));
     }
 
-    private void responseToken(final HttpServletResponse response, final Long userId) throws IOException {
-        final Token token = authTokenService.saveAndGetToken(userId);
-        tokenProvider.responseToken(response, token);
-    }
-
-    private void responseTokenAndError(final HttpServletResponse response, final Long userId) throws IOException {
-        final Token token = authTokenService.saveAndGetToken(userId);
-        tokenProvider.addTokenToCookie(response, token);
-        ResponseUtil.responseError(response, objectMapper, ResponseCode.NOT_YET_SIGNED_UP);
+    private String signUpRequiredQueryParam(final boolean result) {
+        return String.format("?sign-up-required=%s", result);
     }
 }
