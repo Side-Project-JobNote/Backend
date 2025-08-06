a586d81622a9fff6ebb17fc623609da8e9a96bc4
hyotatoFrappuccino <hjkiln@naver.com>
2025-08-06 10:45:48 +0900

    config: LocalDateTime 포맷에 맞게 수정

--- src/main/java/com/jobnote/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/jobnote/global/config/swagger/SwaggerConfig.java
@@ -1,12 +1,17 @@
 package com.jobnote.global.config.swagger;
 
+import io.swagger.v3.oas.models.media.StringSchema;
+import jakarta.annotation.PostConstruct;
 import org.springdoc.core.customizers.OperationCustomizer;
+import org.springdoc.core.utils.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.HandlerMethod;
 
 import io.swagger.v3.oas.models.Operation;
 import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,5 +27,13 @@ public class SwaggerConfig {
 			apiErrorResponseHandler.handleApiErrorResponse(operation, handlerMethod);
 			return operation;
 		};
+	}
+
+	@PostConstruct
+	public void customizeLocalDateTime() {
+		SpringDocUtils.getConfig().replaceWithSchema(LocalDateTime.class,
+				new StringSchema()
+						.example("2025-08-06T00:00:00")
+						.format("date-time"));
 	}
 }
