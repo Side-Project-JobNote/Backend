64b1ca0ac0f85c9f60a2cb41218a9068d4e9d7f5
gnsdp99 <rladpgns99@gmail.com>
2025-08-10 00:52:30 +0900

    test: VerificationEmailService 단위 테스트

--- src/test/java/com/jobnote/domain/email/service/VerificationEmailServiceTest.java
+++ src/test/java/com/jobnote/domain/email/service/VerificationEmailServiceTest.java
@@ -1,16 +1,19 @@
 package com.jobnote.domain.email.service;
 
 import com.jobnote.ServiceUnitTest;
+import com.jobnote.domain.email.domain.VerificationEmailType;
 import com.jobnote.domain.user.domain.User;
 import com.jobnote.domain.email.domain.VerificationEmail;
 import com.jobnote.domain.email.domain.VerificationEmailStatus;
 import com.jobnote.domain.email.repository.VerificationEmailRepository;
+import com.jobnote.domain.user.event.EmailVerificationEvent;
 import com.jobnote.global.exception.JobNoteException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.context.ApplicationEventPublisher;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -18,13 +21,18 @@ import java.util.Optional;
 import static com.jobnote.global.common.ResponseCode.NOT_FOUND_VERIFICATION_EMAIL;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
 
 class VerificationEmailServiceTest extends ServiceUnitTest {
 
     @Mock
     private VerificationEmailRepository verificationEmailRepository;
+
+    @Mock
+    private ApplicationEventPublisher eventPublisher;
 
     @InjectMocks
     private VerificationEmailService verificationEmailService;
@@ -87,4 +95,24 @@ class VerificationEmailServiceTest extends ServiceUnitTest {
             assertThat(result.getStatus()).isEqualTo(VerificationEmailStatus.VERIFIED);
         }
     }
+
+    @Test
+    @DisplayName("인증 이메일을 전송한다.")
+    void sendVerificationEmail() {
+        // given
+        final User user = mock(User.class);
+        final String token = "testToken";
+        final LocalDateTime currentDate = LocalDateTime.of(2025, 7, 29, 12, 0);
+        final LocalDateTime expiryDate = LocalDateTime.of(2025, 8, 6, 11, 31);
+        final VerificationEmail verificationEmail = VerificationEmail.create(token, user, expiryDate);
+
+        given(verificationEmailRepository.save(any(VerificationEmail.class))).willReturn(verificationEmail);
+
+        // when
+        verificationEmailService.send(user, currentDate, VerificationEmailType.SIGN_UP);
+
+        // then
+        then(eventPublisher).should().publishEvent(EmailVerificationEvent.of(user.getEmail(), verificationEmail.getToken(), VerificationEmailType.SIGN_UP));
+    }
+
 }
