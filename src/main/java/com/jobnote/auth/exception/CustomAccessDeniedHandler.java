bd2677d34fbba903190e9dc7a74ab8bbfbf9d84f
gnsdp99 <rladpgns99@gmail.com>
2025-08-17 12:57:19 +0900

    feat: 인증/인가 실패 핸들러 분리

---
+++ src/main/java/com/jobnote/auth/exception/CustomAccessDeniedHandler.java
@@ -0,0 +1,29 @@
+package com.jobnote.auth.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jobnote.global.common.ResponseCode;
+import com.jobnote.global.util.ResponseUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        log.warn("AuthenticationEntryPoint: ", accessDeniedException);
+        ResponseUtil.responseError(response, objectMapper, ResponseCode.FORBIDDEN);
+    }
+}
--- src/main/java/com/jobnote/auth/exception/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/jobnote/auth/exception/CustomAuthenticationEntryPoint.java
@@ -1,27 +1,17 @@
 package com.jobnote.auth.exception;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jobnote.domain.user.service.AuthTokenService;
 import com.jobnote.global.util.ResponseUtil;
 import com.jobnote.global.common.ResponseCode;
-import com.jobnote.global.exception.JobNoteException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-
-import static com.jobnote.global.common.Constants.*;
-import static com.jobnote.global.common.Constants.COOKIE_NAME_REFRESH_TOKEN;
-import static com.jobnote.global.common.ResponseCode.EXPIRED_TOKEN;
-import static com.jobnote.global.util.CookieUtil.getTokenFromCookie;
-import static com.jobnote.global.util.CookieUtil.invalidateCookie;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -29,25 +19,10 @@ import static com.jobnote.global.util.CookieUtil.invalidateCookie;
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     private final ObjectMapper objectMapper;
-    private final AuthTokenService authTokenService;
 
     @Override
     public void commence(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException authException) throws IOException {
-        final JobNoteException e = (JobNoteException) request.getAttribute(ATTRIBUTE_EXCEPTION);
-        final ResponseCode responseCode = e == null ? ResponseCode.FORBIDDEN : e.getResponseCode();
-
-        log.error("AuthenticationException: ", e == null ? authException : e);
-
-        if (URI_USER_REISSUE.equals(request.getRequestURI())) {
-            response.addHeader(HttpHeaders.SET_COOKIE, invalidateCookie(COOKIE_NAME_ACCESS_TOKEN).toString());
-            response.addHeader(HttpHeaders.SET_COOKIE, invalidateCookie(COOKIE_NAME_REFRESH_TOKEN).toString());
-            SecurityContextHolder.clearContext();
-
-            if (EXPIRED_TOKEN.equals(responseCode)) {
-                authTokenService.invalidate(getTokenFromCookie(request.getCookies(), COOKIE_NAME_REFRESH_TOKEN));
-            }
-        }
-
-        ResponseUtil.responseError(response, objectMapper, responseCode);
+        log.warn("AuthenticationEntryPoint: ", authException);
+        ResponseUtil.responseError(response, objectMapper, ResponseCode.UNAUTHORIZED);
     }
 }
