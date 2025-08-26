7a3b1f1f4e5fbc657c7e87439adfaa08dc299516
gnsdp99 <rladpgns99@gmail.com>
2025-08-26 20:36:08 +0900

    config: FrontendProperties 클래스 생성

---
+++ src/main/java/com/jobnote/global/config/properties/FrontendProperties.java
@@ -0,0 +1,14 @@
+package com.jobnote.global.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "frontend")
+public record FrontendProperties(
+        String baseUrl,
+        String mainPage,
+        String socialSignUpPage,
+        String loginFailPage,
+        String resetPasswordPage
+) {
+
+}
