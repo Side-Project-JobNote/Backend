1d2a2281e853ec7e65f8fd723589445e412afe5c
hyotatoFrappuccino <hjkiln@naver.com>
2025-08-30 00:00:01 +0900

    refactor: 책임 분리

--- src/main/java/com/jobnote/domain/applicationformdocument/service/ApplicationFormDocumentService.java
+++ src/main/java/com/jobnote/domain/applicationformdocument/service/ApplicationFormDocumentService.java
@@ -14,6 +14,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static com.jobnote.global.common.ResponseCode.NOT_FOUND_DOCUMENT;
@@ -59,6 +61,33 @@ public class ApplicationFormDocumentService {
             Document document = getByIdOrThrow(request.documentId());
             ApplicationFormDocument entity = request.toEntity(form, document);
             applicationFormDocumentRepository.save(entity);
+        }
+    }
+
+    /* UPDATE */
+    @Transactional
+    public void updateAll(final Long userId, final ApplicationForm form, final List<ApplicationFormDocumentRequest> requests) {
+        // 기존 연결된 문서 조회
+        List<ApplicationFormDocument> existsDocuments = getAllByApplicationFormId(userId, form.getId());
+
+        // 요청 문서 ID 목록
+        Set<Long> requestsIds = requests.stream()
+                .map(ApplicationFormDocumentRequest::id)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
+
+        // 삭제 : 기존 문서 중 요청에 없는 문서 삭제
+        for (ApplicationFormDocument document : existsDocuments) {
+            if (!requestsIds.contains(document.getId())) {
+                delete(document);
+            }
+        }
+
+        // 신규 생성
+        for (ApplicationFormDocumentRequest req : requests) {
+            if (req.id() == null) {
+                saveAll(form, List.of(req));
+            }
         }
     }
 
